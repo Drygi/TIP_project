09e59f34e605ec5b28e12d98c99d365bd8475e70
Drygi <mikolaj_drygas@tlen.pl>
2017-05-07 19:06:47 +0200

    1.8.1

--- Client/Menu.xaml.cs
+++ Client/Menu.xaml.cs
@@ -33,18 +33,13 @@ namespace Client
         private WaveIn waveSource;
         private WaveFileWriter waveFile;
         private WaveOut waveOut;
-        private Stream output;
 
         public Menu()
         {
             InitializeComponent();
             loginName.Text += GlobalMemory._user.login;
-            waveOut = new WaveOut();
-            startListening();
-            
-
+            initialize();
         }
-     
         private async void LogoutButton_Click(object sender, RoutedEventArgs e)
         {
             if (Helper.GlobalHelper.messageBoxYesNO("Czy na pewno chcesz się wylogować?"))
@@ -59,7 +54,6 @@ namespace Client
                     MessageBox.Show("Coś poszło nie tak");
             }
         }
-
         private async void onlineUsers_Click(object sender, RoutedEventArgs e)
         {
             listBoxItems.ItemsSource = null;
@@ -71,31 +65,19 @@ namespace Client
                 listBoxItems.ItemsSource = Helper.GlobalMemory.onlineUsers;
 
         }
-
         private void callButton_Click(object sender, RoutedEventArgs e)
         {
             if (listBoxItems.SelectedIndex == -1)
                 MessageBox.Show("Nie wybrano żadnego użytkownika");
             else
                 if (client == null)
-            {
-                client = UdpUser.ConnectTo(GlobalMemory.onlineUsers[listBoxItems.SelectedIndex].ipAddress, 32123);
-                sendVoice();
-                 //client.Send("INVITE");
-            }
-            else
-                MessageBox.Show("Nie możesz prowadzić dwóch rozmów na raz, najpierw zakończ obecną rozmowę!");
+                {
+                    client = UdpUser.ConnectTo(GlobalMemory.onlineUsers[listBoxItems.SelectedIndex].ipAddress, 32123);
+                    client.Send("INVITE");
+                }
+                else
+                    MessageBox.Show("Nie możesz prowadzić dwóch rozmów na raz, najpierw zakończ obecną rozmowę!");
         }
-
-        public void removeAcutalUserFromList(User user)
-        {
-            for (int i = 0; i < Helper.GlobalMemory.onlineUsers.Count; i++)
-            {
-                if (Helper.GlobalMemory.onlineUsers[i].login.ToUpper() == user.login.ToUpper())
-                    Helper.GlobalMemory.onlineUsers.RemoveAt(i);
-            }
-        }
-
         private async void deleteButton_Click(object sender, RoutedEventArgs e)
         {
             if (Helper.GlobalHelper.messageBoxYesNO("Czy na pewno chcesz usunąć swoje konto?"))
@@ -112,50 +94,48 @@ namespace Client
                     MessageBox.Show("Coś poszło nie tak");
             }
         }
-
-
         private void callEndButton_Click(object sender, RoutedEventArgs e)
         {
-            if(Helper.GlobalHelper.messageBoxYesNO("Czy na pewno chcesz zakończyć rozmowę?"))
-            {
-              //  client.Send("BYE");
-             //   callEndButton.IsEnabled = false;
-                waveSource.StopRecording();
-                //  client = null;
-            }
-
+            if (Helper.GlobalHelper.messageBoxYesNO("Czy na pewno chcesz zakończyć rozmowę?"))
+                client.Send("BYE");
         }
 
-        private void startListening()
+        private void initialize()
         {
-            waveFile = null;
-            waveSource = null;
-           
-            var server = new UdpListener(new IPEndPoint(IPAddress.Parse(GlobalMemory._user.ipAddress), 32123));
+            waveOut = new WaveOut();
+            waveSource = new WaveIn();
+            waveSource.WaveFormat = new WaveFormat(44100, 1);
+            waveSource.DataAvailable += new EventHandler<WaveInEventArgs>(waveSource_DataAvailable);
+            waveSource.RecordingStopped += new EventHandler<StoppedEventArgs>(waveSource_RecordingStopped);
+            waveFile = new WaveFileWriter("call.wav", waveSource.WaveFormat);
 
-            //start listening for messages and copy the messages back to the client
-            Task.Factory.StartNew(async() => {
+            var server = new UdpListener(new IPEndPoint(IPAddress.Parse(GlobalMemory._user.ipAddress), 32123));
+            Task.Factory.StartNew(async() => 
+            {
                     while (true)
                     {
-                       // var received = await server.Receive();
-                        //messageCase(received);
+                        var received = await server.Receive();
+                        messageCase(received);
+                        
                         var receivedVoice = await server.ReceiveVoice();
-                        Console.WriteLine("LOL");
-                        playVoice(receivedVoice);
-
-                    //  if (received.Message == "quit")
-                    //        break;
+                        playVoice(receivedVoice);     
                     }
-                                        });
+            });
 
+        }
+        private void removeAcutalUserFromList(User user)
+        {
+            for (int i = 0; i < Helper.GlobalMemory.onlineUsers.Count; i++)
+            {
+                if (Helper.GlobalMemory.onlineUsers[i].login.ToUpper() == user.login.ToUpper())
+                    Helper.GlobalMemory.onlineUsers.RemoveAt(i);
+            }
         }
         private void playVoice(ReceivedVoice _receivedvoice)
         {
             IWaveProvider provider = new RawSourceWaveStream(_receivedvoice.Message, new WaveFormat());
-            waveOut.Init(provider);
-           
+            waveOut.Init(provider); 
             waveOut.Play();
-            
         }
         private void messageCase(Received _received)
         {
@@ -170,28 +150,28 @@ namespace Client
                         if (GlobalHelper.messageBoxYesNO("Dzwoni " + _login + " czy chcesz odebrać?"))
                         {
                             client.Send("ACK");
-                            sendVoice();
+                            waveSource.StartRecording();
                         }
                         else
                         {
                             client.Send("CANCEL");
                             client = null;
                         }
-                        break;
+                    break;
                 }
                 case "ACK":
                 {
-                         // tutaj bedzie trzeba zrobić jakąs animacje ze rozmowa jest
+                        // tutaj bedzie trzeba zrobić jakąs animacje ze rozmowa jest
+                        waveSource.StartRecording();
                         MessageBox.Show("Połączenie zostało odebrane");
-                        sendVoice();
                     break;
                 }
                 case "BYE":
                 {
-                        MessageBox.Show("Połączenie zostało zakończone");
-                        callEndButton.IsEnabled = false;
+                        client.Send("BYE");
                         waveSource.StopRecording();
                         client = null;
+                        MessageBox.Show("Połączenie zostało zakończone");
                     break;
                 }
                 case "CANCEL":
@@ -206,96 +186,22 @@ namespace Client
             }
         }
 
-  
-        void sendVoice()
+       private void waveSource_DataAvailable(object sender, WaveInEventArgs e)
         {
-            callButton.IsEnabled = false;
-
-            callEndButton.IsEnabled = true;
-
-            waveSource = new WaveIn();
-            waveSource.WaveFormat = new WaveFormat(44100, 1);
-
-            waveSource.DataAvailable += new EventHandler<WaveInEventArgs>(waveSource_DataAvailable);
-            waveSource.RecordingStopped += new EventHandler<StoppedEventArgs>(waveSource_RecordingStopped);
-            //zapis dzwieku
-            waveFile = new WaveFileWriter("Test0001.wav", waveSource.WaveFormat);
-
-            
-           // waveFile = new WaveFileWriter(output, waveSource.WaveFormat);
-            waveSource.StartRecording();
-
-        }
- 
-        void waveSource_DataAvailable(object sender, WaveInEventArgs e)
-        {
-
-            if (waveFile != null)
-            {
-               
-                waveFile.Write(e.Buffer, 0, e.BytesRecorded);
+            if (waveFile != null)  
                 client.SendBytes(e.Buffer);
-                waveFile.Flush();
-
-                
-            }
         }
-
-        void waveSource_RecordingStopped(object sender, StoppedEventArgs e)
+        private void waveSource_RecordingStopped(object sender, StoppedEventArgs e)
         {
             if (waveSource != null)
             {
                 waveSource.Dispose();
                 waveSource = null;
             }
-
             if (waveFile != null)
             {
                 waveFile.Dispose();
                 waveFile = null;
-            }
-
-            callButton.IsEnabled = true;
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            output = new WaveFileReader("Test0001.wav");
-            sendInParts(output);
-            // client.SendBytes(ReadFully(output));
-            //sendVoice();
-        }
-        private static byte[] ReadFully(Stream input)
-        {
-            byte[] buffer = new byte[16 *1024];
-            using (MemoryStream ms = new MemoryStream())
-            {
-                int read;
-                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
-                {
-                    ms.Write(buffer, 0, read);
-                }
-                return ms.ToArray();
-            }
-        }
-
-        private void sendInParts(Stream input)
-        {
-            byte[] buffer =  ReadFully(input);
-            byte[] buffer2 = new byte[6500];
-            int counter = 0;
-
-            for (int i = 0; i < buffer.Length; i++)
-            {
-
-                buffer2[counter] = buffer[i];
-                counter++;
-                if (counter == 6500 || i==buffer.Length-1)
-                {
-                    counter = 0;
-                    client.SendBytes(buffer2);
-                    buffer2 = new byte[6500];
-                }
             }
         }
     }
